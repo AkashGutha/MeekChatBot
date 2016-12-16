3a4ca9c45693b93754daba750cbc4a76100daead
AkashGutha <akash.gutha@outlook.com>
2016-12-16 16:47:14 +0530

    setup simple follow reply bot

---
+++ bot.js
@@ -0,0 +1,32 @@
+var twit = require('twit');
+var config = require('./config.json');
+
+var Twitter = new twit(config);
+
+// set up a user stream
+var stream = Twitter.stream('user');
+
+stream.on("follow", onFollow);
+
+// call backs
+function onFollow(event) {
+    //get user's twitter handler (screen name)
+    var name = event.source.name;
+    var screenName = event.source.screen_name;
+    console.log('followed by @' + screenName);
+    // function that replies back to the user who followed
+    tweetNow('@' + screenName + ' Welcome to Meek Chat. Happy to see you around.');
+}
+
+function tweetNow(tweetTxt) {
+    var tweet = {
+        status: tweetTxt
+    }
+    Twitter.post('statuses/update', tweet, function (err, data, response) {
+        if (err) {
+            console.log("Error in Replying");
+        } else {
+            console.log("Gratitude shown successfully");
+        }
+    });
+}
