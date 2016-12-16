6841a0937a26fa278fc93e69f9ec69fa940b47cd
AkashGutha <akash.gutha@outlook.com>
2016-12-16 22:20:43 +0530

    add follow function

--- bot.js
+++ bot.js
@@ -5,14 +5,14 @@ var Twitter = new twit(config);
 
 // set up a user stream
 var stream = Twitter.stream('user');
-// listen for events on this stream.
-stream.on("follow", onFollow);
-
-
 
 // main program start.
 
 welcomeMsg();
+
+// listen for events on this stream.
+stream.on("follow", onFollow);
+stream.on('tweet', onTweet);
 
 // main program end.
 
@@ -26,6 +26,14 @@ function onFollow(event) {
 	// function that replies back to the user who followed
 	tweetNow('@' + screenName + ` Thanks for following Meek Chat. Happy to see you around.
 	We will keep you posted about future updates.`);
+}
+
+function onTweet(event) {
+	//get user's twitter handler (screen name)
+	var name = event.source.name;
+	var screenName = event.source.screen_name;
+	// function that follows the user who tweeetedif not following
+
 }
 
 
@@ -45,6 +53,19 @@ function tweetNow(tweetTxt) {
 		status: tweetTxt
 	}
 	Twitter.post('statuses/update', tweet, function (err, data, response) {
+		if (err) {
+			console.log(err.message);
+		}
+	});
+}
+
+
+function follow(screenName, id) {
+	var query = {
+		screen_name: screenName,
+		user_id: id
+	}
+	Twitter.post('friendships/create', query, function (err, data, response) {
 		if (err) {
 			console.log(err.message);
 		}
