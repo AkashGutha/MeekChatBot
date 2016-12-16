1c29c26a1a01f4ef43dc1673022058994898ac44
AkashGutha <akash.gutha@outlook.com>
2016-12-16 21:17:06 +0530

    streamline code

--- bot.js
+++ bot.js
@@ -5,24 +5,39 @@ var Twitter = new twit(config);
 
 // set up a user stream
 var stream = Twitter.stream('user');
-
+// listen for events on this stream.
 stream.on("follow", onFollow);
 
-console.log('===========================================');
-console.log('Meek chat bot started succesfully');
-console.log('===========================================');
-console.log('');
-console.log('Listening for events ...');
-console.log('');
+
+
+// main program start.
+
+welcomeMsg();
+
+// main program end.
+
+
+// callback functions
 
 function onFollow(event) {
-	console.log('event');
 	//get user's twitter handler (screen name)
 	var name = event.source.name;
 	var screenName = event.source.screen_name;
-	console.log('you have been followed by @' + screenName);
 	// function that replies back to the user who followed
-	tweetNow('@' + screenName + ' Thanks for following Meek Chat. Happy to see you around.');
+	tweetNow('@' + screenName + ` Thanks for following Meek Chat. Happy to see you around.
+	We will keep you posted about future updates.`);
+}
+
+
+// helper functions
+
+function welcomeMsg() {
+	console.log('===========================================');
+	console.log('Meek chat bot started succesfully');
+	console.log('===========================================');
+	console.log('');
+	console.log('Listening for events ...');
+	console.log('');
 }
 
 function tweetNow(tweetTxt) {
@@ -31,10 +46,7 @@ function tweetNow(tweetTxt) {
 	}
 	Twitter.post('statuses/update', tweet, function (err, data, response) {
 		if (err) {
-			console.log(err);
-			console.log("Error in Replying");
-		} else {
-			console.log("Gratitude shown successfully");
+			console.log(err.message);
 		}
 	});
 }
