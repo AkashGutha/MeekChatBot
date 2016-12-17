e11ef552844d9dc25c348ab5d6b9659a07474bde
AkashGutha <akash.gutha@outlook.com>
2016-12-17 16:37:17 +0530

    streamline code acording to library

--- bot.js
+++ bot.js
@@ -1,75 +1,20 @@
-var twit = require('twit');
-var config = require('./config.json');
+// set up twiiter app with configurations.
+var Twitter = require('./twitter');
 
-var Twitter = new twit(config);
+// import required library
+var Follow = require('./lib/follow');
+
+// include helper library for predefined messages.
+var helperMessages = require('./heplers/messages');
 
 // set up a user stream
 var stream = Twitter.stream('user');
 
 // main program start.
 
-welcomeMsg();
+helperMessages.welcomeMsg();
 
 // listen for events on this stream.
-stream.on("follow", onFollow);
-stream.on("tweet", onTweet);
+stream.on('follow', Follow.gratitudeOnFollow);
 
 // main program end.
-
-
-// callback functions
-
-function onFollow(event) {
-	//get user's twitter handler (screen name)
-	var name = event.source.name;
-	var screenName = event.source.screen_name;
-	var user_id = event.source.user_id;
-	// function that replies back to the user who followed
-	tweetNow('@' + screenName + ` Thanks for following Meek Chat. Happy to see you around.
-	We will keep you posted about future updates.`);
-	follow(screenName, user_id);
-}
-
-function onTweet(event) {
-	//get user's twitter handler (screen name)
-	var name = event.source.name;
-	var screenName = event.source.screen_name;
-	// function that follows the user who tweeetedif not following
-
-}
-
-
-// helper functions
-
-function welcomeMsg() {
-	console.log('===========================================');
-	console.log('Meek chat bot started succesfully');
-	console.log('===========================================');
-	console.log('');
-	console.log('Listening for events ...');
-	console.log('');
-}
-
-function tweetNow(tweetTxt) {
-	var tweet = {
-		status: tweetTxt
-	}
-	Twitter.post('statuses/update', tweet, function (err, data, response) {
-		if (err) {
-			console.log(err.message);
-		}
-	});
-}
-
-
-function follow(screenName, id) {
-	var query = {
-		screen_name: screenName,
-		user_id: id
-	}
-	Twitter.post('friendships/create', query, function (err, data, response) {
-		if (err) {
-			console.log(err.message);
-		}
-	});
-}
