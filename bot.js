be34bdb5a07a4d0d40444d40356167b9803d02b6
AkashGutha <akash.gutha@outlook.com>
2016-12-17 23:15:56 +0530

    add search functionality and queue

--- bot.js
+++ bot.js
@@ -3,9 +3,16 @@ var Twitter = require('./twitter');
 
 // import required library
 var Follow = require('./lib/follow');
+var Search = require('./lib/search');
 
 // include helper library for predefined messages.
 var helperMessages = require('./heplers/messages');
+
+var queue = [];
+
+// set up timed functions.
+setInterval(search, 30000);
+setInterval(followFromQueue, 1000);
 
 // set up a user stream
 var stream = Twitter.stream('user');
@@ -18,3 +25,21 @@ helperMessages.welcomeMsg();
 stream.on('follow', Follow.gratitudeOnFollow);
 
 // main program end.
+
+function search() {
+    Search.searchByTerm('#gamedev').then(function (data, response) {
+        data.statuses.forEach(function (element) {
+            queue.push({
+                id: element.user.id,
+                screen_name: element.user.screen_name
+            })
+        }, this);
+    });
+}
+
+function followFromQueue() {
+    if (queue !== []) {
+        var user = queue.pop();
+        Follow.follow(user.screen_name, user.id);
+    }
+}
