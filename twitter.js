cd979f135aca403387ab24cad366fe46a4357acd
AkashGutha <akash.gutha@outlook.com>
2016-12-17 20:48:22 +0530

    add support for env variables

--- twitter.js
+++ twitter.js
@@ -1,5 +1,15 @@
 var twit = require('twit');
-var config = require('./config.json');
+
+if (process.env.argv[2] == '--local') {
+    var config = require('./config.json');
+} else {
+    var config = {
+        access_token: process.env.access_token,
+        access_token_secret: process.env.access_token_secret,
+        consumer_key: process.env.consumer_key,
+        consumer_secret: process.env.consumer_secret,
+    }
+}
 
 var Twitter = new twit(config);
 
