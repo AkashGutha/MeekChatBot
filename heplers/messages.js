bf72983968a75319d9a2c86725110f12279a9552
AkashGutha <akash.gutha@outlook.com>
2016-12-17 16:31:51 +0530

    add helper messages

---
+++ heplers/messages.js
@@ -0,0 +1,8 @@
+exports.welcomeMsg = function welcomeMsg() {
+    console.log('===========================================');
+    console.log('Meek chat bot started succesfully');
+    console.log('===========================================');
+    console.log('');
+    console.log('Listening for events ...');
+    console.log('');
+}
